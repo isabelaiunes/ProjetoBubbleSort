f4564d5165e2aa3add87d095e40cef99d6942ace
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 08:59:18 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -16,8 +16,12 @@ lista = [3, 6, 7, 12]
 
 def bubble_sort(arr):
     n = len(arr)
-# Para cada elemento i do array
-for i in range(n):
-    #Para cada elemento j do array
-    for j in range(0, n-i-1)
-    
+    # Para cada elemento i do array
+    for i in range(n):
+        #Para cada elemento j do array
+        for j in range(0, n-i-1):
+            #Se elemento i for maior que elemento j
+            if arr[j] > arr[j+1]:
+                #Troque os elementos i e j
+                arr[j], arr [j+1] = arr[j+1], arr[j]
+    return  arr 
