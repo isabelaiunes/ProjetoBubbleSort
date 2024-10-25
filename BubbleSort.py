9cf6c5b4a3074945aa88a3a1ca6a12b19eb843f8
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 09:15:22 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -1,11 +1,11 @@
 # Início #
 # Loop1: Para cada elemento i no array de tamanho n #
-# Loop2: (compara os elementos de novo) Para cada elemento j no array de tamanho n-1 #
+# Loop2: Para cada elemento j no array de tamanho n-1 (compara os elementos de novo) #
 # Condicional: Se cada elemento i for maior que elemento j, troque os elementos i e j #
 # Exiba o array ordenado #
 # Fim #
 
-# Considere esta lista como exemplo para ordenação com Bubble Sort
+# Considere esta lista como exemplo para ordenação com Bubble Sort #
 lista = [6, 3, 12, 7]
 # Na primeira vez que o algoritmo passa, se compara elemento i e j e coloca o menor em primeiro lugar #
 lista = [3, 6, 12, 7]
