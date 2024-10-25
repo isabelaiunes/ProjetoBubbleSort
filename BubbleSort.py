5e7a93fd58d50c6b2c9d885fe4a9ae5d689b25b6
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 08:36:03 -0300

    master

---
+++ BubbleSort.py
@@ -0,0 +1,6 @@
+# Inicie #
+# Loop1: Para cada elemento i no array de tamanho n #
+# Loop2: (compara os elementos de novo) Para cada elemento j no array de tamanho n-1 #
+# Condicional: Se cada elemento i for maior que elemento j, troque os elementos i e j #
+# Exiba o array ordenado #
+# Fim #
