3b312deb340d8e4086863658d9043aee5bc1fbbd
Isabela Iunes <iunesisabela.cs@gmail.com>
2024-10-25 08:40:42 -0300

    master

--- BubbleSort.py
+++ BubbleSort.py
@@ -4,3 +4,8 @@
 # Condicional: Se cada elemento i for maior que elemento j, troque os elementos i e j #
 # Exiba o array ordenado #
 # Fim #
+
+# Considere esta lista como exemplo para ordenação com Bubble Sort
+lista = [6, 3, 12, 7]
+# Na primeira vez que o algoritmo passa, se compara elemento i e j e coloca o menor em primeiro lugar #
+lista = [3, 6, 12, 7]
